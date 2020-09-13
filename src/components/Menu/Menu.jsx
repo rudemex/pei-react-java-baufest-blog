92fca0cd8755f041586ce227cdac0965bf11db95
Maximiliano Delgado <mdelgado@baufest.com>
2020-09-13 10:42:03 -0300

    menu

---
+++ src/components/Menu/Menu.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
+//import { Link } from 'react-router-dom';
+
+const Menu = () => {
+  return (
+    <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
+      <div className="container">
+        <Navbar.Brand>Baufest Blog</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link to="/About">About</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </div>
+    </Navbar>
+  );
+}
+
+export default Menu;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import About from './containers/About';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./assets/css/app.css";
 
 ReactDOM.render(<About/>,document.getElementById("app"));
