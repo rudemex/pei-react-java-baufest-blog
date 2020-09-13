1a60bebbc06804c525daf6dc863a44e4254f7cb6
Maximiliano Delgado <mdelgado@baufest.com>
2020-09-13 13:51:34 -0300

    states

--- src/containers/Post.jsx
+++ src/containers/Post.jsx
@@ -3,16 +3,36 @@ import React, { Component } from 'react';
 class Post extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      id: null,
+    };
   }
   componentDidMount() {
-    console.log(this.props);
+    this.setState((prevState) => ({
+      ...prevState,
+      id: this.props.match.params.id,
+    }));
   }
 
+  updateState = () => {
+    setTimeout(() => {
+      this.setState((prevState) => ({
+        ...prevState,
+        id: 8,
+      }));
+    }, 5000);
+  };
+
   render() {
-    return <div>
-      <h1>Post</h1>
-      <p>Este es el posteo: {this.props.match.params.id}</p>
-    </div>;
+    const { id } = this.state;
+
+    return (
+      <div>
+        <h1>Post</h1>
+        <p>Este es el posteo: {id}</p>
+        {this.updateState()}
+      </div>
+    );
   }
 }
 
